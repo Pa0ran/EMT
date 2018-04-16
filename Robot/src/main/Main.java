60808d3f9e1b3f09b4377691030d776d3f8adff6
Niklas <niklas.malinen@metropolia.fi>
2018-04-16 14:10:57 +0300

    Main changes

--- Robot/src/main/Main.java
+++ Robot/src/main/Main.java
@@ -3,13 +3,15 @@ package main;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.RegulatedMotor;
 
 public class Main {
-	static EV3IRSensor Sensor;
+	
 	public static void main(String[] args) {
-		IRSensor irs = new IRSensor(Sensor);;
+		EV3IRSensor irSensor = new EV3IRSensor(SensorPort.S4);
+		IRSensor irs = new IRSensor(irSensor);;
 		RegulatedMotor m1 = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor m2 = new EV3LargeRegulatedMotor(MotorPort.B);
 		Motor moottori = new Motor(m1,m2); 
