ec455f495464d44fec71be272f70e59fa809bfca
ilirjanz <ilirjanz@metropolia.fi>
2018-04-26 11:34:06 +0300

    Update JaateloValinta.java

--- Robot/src/main/JaateloValinta.java
+++ Robot/src/main/JaateloValinta.java
@@ -1,8 +1,3 @@
-
-
-
-
-
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 
@@ -11,64 +6,89 @@ public class JaateloValinta {
 	double jaatelo2;
 	double jaatelo3;
 	double kassaSaldo;
-	
-	public JaateloValinta(double jaatelo1, double jaatelo2, double jaatelo3, double kassaSaldo){
+
+	public JaateloValinta(double jaatelo1, double jaatelo2, double jaatelo3, double kassaSaldo) {
 		this.jaatelo1 = 2.50;
 		this.jaatelo2 = 3.25;
 		this.jaatelo3 = 1.50;
 		this.kassaSaldo = 10.50;
-		
-		String jaatelot[] = {"Mansikkajäätelo", "Suklaajäätelö", "Lakritsijäätelö"};
-		
-		for(int i=0; i< jaatelot.length; i++){
-			
-			LCD.drawString("Päivää",0,0);
-			LCD.drawString("Mitä saisi olla?",0,0);
-			LCD.drawString("Tarjolla olisi mansikkajäätelöä, suklaajäätelöa ja lakritsijäätelöä.",0,0);
-		    LCD.drawString("Jos haluat ostaa jäätelöä paina Enter näppäintä",0,0);
-		     
-		    if(Button.ENTER.isDown()) {
-			
-			  while(!Button.ESCAPE.isDown()) {
-			    	
-			    	if(Button.LEFT.isDown()) {
-			    		kassaSaldo = kassaSaldo + jaatelo1;
-			    	}else if(Button.RIGHT.isDown()) {
-			    		kassaSaldo = kassaSaldo + jaatelo2;
-			    	}else if(Button.DOWN.isDown()) {
-			    		kassaSaldo = kassaSaldo +jaatelo3;    	
-			        }else if(Button.UP.isDown()) {
-			        	LCD.drawString("Do you want to buy more ice cream?",0,0);
-			        	LCD.drawString("If you do, press button ENTER",0,0);
-			        	LCD.drawString("If you want to quit, press ESCAPE button",0,0);
-			        	if(Button.ENTER.isDown()) {
-			        		continue;
-			         	}if(Button.ESCAPE.isDown()) {
-			         		break;
-			         	}
-			        		
-			        }
-			    	
-			    		LCD.drawString("Kassan saldo:"+kassaSaldo,0,0);
-			    	
-			    	
-			    }
-		    }
-		
-		for(String menu: jaatelot) {
-			
-			
-			
+
+		String jaatelot[] = { "Mansikkajäätelo", "Suklaajäätelö", "Lakritsijäätelö" };
+
+		for (int i = 0; i < jaatelot.length; i++) {
+
+			LCD.drawString("Päivää", 0, 0);
+			LCD.drawString("Mitä saisi olla?", 0, 0);
+			LCD.drawString("Tarjolla olisi mansikkajäätelöä, suklaajäätelöa ja lakritsijäätelöä.", 0, 0);
+			LCD.drawString("Jos haluat ostaa jäätelöä paina Enter näppäintä", 0, 0);
+			LCD.clear();
+
+			while (!Button.ESCAPE.isDown()) {
+				if (Button.DOWN.isDown()) {
+					i++;
+					LCD.drawString("Mansikkajäätelö", 0, 0);
+					if (Button.ENTER.isDown()) {
+						kassaSaldo = kassaSaldo + jaatelo1;
+
+					}
+					if (Button.DOWN.isDown()) {
+						i++;
+						LCD.drawString("Suklaajäätelö", 0, 0);
+						if (Button.ENTER.isDown()) {
+							kassaSaldo = kassaSaldo + jaatelo2;
+						}
+						if (Button.DOWN.isDown()) {
+							i++;
+							LCD.drawString("Lakritsijäätelö", 0, 0);
+							if (Button.ENTER.isDown()) {
+								kassaSaldo = kassaSaldo + jaatelo3;
+							}
+							if (Button.DOWN.isDown()) {
+								i++;
+								LCD.drawString("Tulisiko vielä muuta?", 0, 0);
+								LCD.drawString("Jos tulee, niin paina Enteriä", 0, 0);
+								LCD.drawString("Jos ei, paina ESCAPE näppäintä", 0, 0);
+								if (Button.ENTER.isDown()) {
+									continue;
+								}
+								
+							}
+							break;
+						}
+
+					}
+					/*
+					 * if (Button.UP.isDown() || i > 1) { i--; }
+					 */
+
+					/*
+					 * while (!Button.ESCAPE.isDown()) {
+					 * 
+					 * if (Button.LEFT.isDown()) { 
+					 * // LCD.drawString("Mansikkajäätelö", 0, 0);
+					 * kassaSaldo = kassaSaldo + jaatelo1; 
+					 * LCD.clear(); }else if (Button.RIGHT.isDown()) { // LCD.drawString("Suklaajäätelö", 0, 0);
+					 * kassaSaldo = kassaSaldo + jaatelo2; LCD.clear(); } else if
+					 * (Button.DOWN.isDown()) { // LCD.drawString("Lakritsijäätelö", 0, 0);
+					 * kassaSaldo = kassaSaldo + jaatelo3; LCD.clear(); } else if
+					 * (Button.UP.isDown()) { LCD.drawString("Do you want to buy more ice cream?",
+					 * 0, 0); LCD.drawString("If you do, press button ENTER", 0, 0);
+					 * LCD.drawString("If you want to quit, press ESCAPE button", 0, 0); if
+					 * (Button.ENTER.isDown()) { continue; } if (Button.ESCAPE.isDown()) { break; }
+					 * 
+					 * }
+					 * 
+					 * LCD.drawString("Kassan saldo:" + kassaSaldo, 0, 0);
+					 * 
+					 * }
+					 */
+				}
+
+				// for (String menu : jaatelot) {
+
+				// }
+
+			}
 		}
-		
-
-	
-	}}}
-	
-	
-	
-
-
-	
-	
-
+	}
+}
