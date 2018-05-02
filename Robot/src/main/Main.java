8d13d678bb758a582b81e5b5928160166acd1759
Niklas <niklas.malinen@metropolia.fi>
2018-05-02 15:23:36 +0300

    Kuvat toimii

--- Robot/src/main/Main.java
+++ Robot/src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
+import lejos.hardware.BrickFinder;
 import lejos.hardware.Button;
+import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
@@ -9,6 +11,7 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
 public class Main {
 	
@@ -16,12 +19,16 @@ public class Main {
 	RouteManager rM;
 	Motor moottori;
 	
+	
+	
 	public static void main(String[] args) {
 		
 	
 		
-		
-		
+		GraphicsLCD  g = BrickFinder.getDefault().getGraphicsLCD();
+		g.setAutoRefresh(false);
+		final int SW = g.getWidth();
+		final int SH = g.getHeight();
 		//motors
 		RegulatedMotor m1 = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor m2 = new EV3LargeRegulatedMotor(MotorPort.B);
@@ -43,14 +50,18 @@ public class Main {
 		
 		//Motor moottori = rM.moottori;
 		//irs.start();
+		int i = 1;
 		while(!Button.ENTER.isDown()) {
-			if(cs.Stop == false) {
+			
+			g.drawRegion(Images.getImage(i), 0, 0, SW, SH, GraphicsLCD.TRANS_NONE, SW/2, SH/2, GraphicsLCD.HCENTER | GraphicsLCD.VCENTER);
+			g.refresh();
+			/*if(cs.Stop == false) {
 			moottori.drive(controls.getMotorSpeed());
 			moottori.steer(controls.getSteeringAngle());
 			}
 			else {
 				moottori.drive(0);
-			}
+			}*/
 			
 			
 			
