434769f433fd20557d6b06e82af719d35305af77
ilirjanz <ilirjanz@metropolia.fi>
2018-05-03 12:32:33 +0300

    Update JaateloValinta.java

--- Robot/src/main/JaateloValinta.java
+++ Robot/src/main/JaateloValinta.java
@@ -18,21 +18,23 @@ public class JaateloValinta {
 		int i = 0;
 
 		while (!Button.ESCAPE.isDown()) {
-			LCD.drawString("Hello", 0, 0);
-			LCD.drawString("Mita saisi olla)", 0, 0);
-			LCD.drawString("Tarjolla olisi mansikkajaateloa, suklaajaateloa ja lakritsijaateloa.", 0, 0);
-			LCD.drawString("Jos haluat ostaa jaateloa paina Enter nappainta", 0, 0);
+			LCD.drawString("Hello /n", 0, 0);
+			LCD.drawString("Mita saisi olla/n", 0, 0);
+			LCD.drawString("Tarjolla olisi mansikkajaateloa, suklaajaateloa ja lakritsijaateloa./n", 0, 0);
+			LCD.drawString("Jos haluat ostaa jaateloa paina Enter nappainta/n", 0, 0);
 			LCD.clear();
 
 			switch (i) {
 
 			case 1:
-				LCD.drawString("Jaatelo1", 0, 0);	
+				LCD.drawString("Jaatelo1", 0, 0);
 				if (Button.ENTER.isDown()) {
 					kassaSaldo = kassaSaldo + jaatelo1;
 
-				} else if(Button.DOWN.isDown()){
+				} else if (Button.DOWN.isDown()) {
 					i++;
+				} else if (Button.UP.isDown()) {
+					i--;
 				}
 				break;
 			case 2:
@@ -40,8 +42,10 @@ public class JaateloValinta {
 				if (Button.ENTER.isDown()) {
 					kassaSaldo = kassaSaldo + jaatelo2;
 
-				} else if(Button.DOWN.isDown()){
+				} else if (Button.DOWN.isDown()) {
 					i++;
+				} else if (Button.UP.isDown()) {
+					i--;
 				}
 
 				break;
@@ -50,27 +54,30 @@ public class JaateloValinta {
 				if (Button.ENTER.isDown()) {
 					kassaSaldo = kassaSaldo + jaatelo3;
 
-				} else if(Button.DOWN.isDown()){
-					i++;
+				} else if (Button.DOWN.isDown()) {
+					i = 0;
+				} else if (Button.UP.isDown()) {
+					i--;
 				}
+
 				break;
 			default:
 
-					LCD.drawString("Tulisiko viela muuta?", 0, 0);
-					LCD.drawString("Jos tulee, niin paina Enteria", 0, 0);
-					LCD.drawString("Jos ei, paina ESCAPE nappainta", 0, 0);
-					if (Button.ENTER.isDown()) {
-						continue;
-					}
-					if (Button.ESCAPE.isDown()) {
-						break;
-					}
-
+				LCD.drawString("Tulisiko viela muuta?", 0, 0);
+				LCD.drawString("Jos tulee, niin paina Enteria", 0, 0);
+				LCD.drawString("Jos ei, paina ESCAPE nappainta", 0, 0);
+				if (Button.ENTER.isDown()) {
+					continue;
+				}
+				if (Button.ESCAPE.isDown()) {
+					break;
 				}
 
-				break;
 			}
 
+			break;
 		}
 
 	}
+
+}
