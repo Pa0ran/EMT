5ca1b0b6fa8603425592ef0a3b4ecbb680b21280
SamHemming <samhem@metropolia.fi>
2018-05-03 13:05:13 +0300

    ColorSensor can now call jaatelo

--- Robot/src/main/ColorSensor.java
+++ Robot/src/main/ColorSensor.java
@@ -1,6 +1,7 @@
 package main;
 
 import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.Color;
 
@@ -8,7 +9,8 @@ public class ColorSensor extends Thread {
 	private EV3ColorSensor cs;
 	private Motor motor;
 	public boolean Stop;
-	
+	public boolean shopOpen = false;
+	private JaateloValinta jaatelo;
 	
 	public ColorSensor(EV3ColorSensor colorSensor, Motor motor) {
 		this.cs = colorSensor;
@@ -23,6 +25,15 @@ public class ColorSensor extends Thread {
 			case(Color.BLACK):
 				break;
 			case(Color.BLUE):
+				//----------------------------------------------------------------------------
+				if (!shopOpen) {
+					LCD.drawString("I'm seeing BLUE!!!", 0, 2); //for debug
+					shopOpen = true; //prevents shop from opening twice
+					jaatelo = new JaateloValinta(1, 2, 3, 10); //for reference
+					shopOpen = false; //assuming that the code is stuck in the "jaatelo" for its execution time
+					//change shopOpen to false before closing the shop so that it can't open twice
+				}
+				//-----------------------------------------------------------------------------
 				break;
 			case(Color.BROWN):
 				break;
